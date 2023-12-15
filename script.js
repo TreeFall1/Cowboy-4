3832b7c102ab80dc7257ca0b0b0ba9085865ac48
TreeFall1 <andreevdaniil06an@gmail.com>
2023-12-15 15:09:41 +0300

    Code optimization

--- script.js
+++ script.js
@@ -41,20 +41,43 @@ colorButton__black.addEventListener("click", bikeToBlack)
 
 const scrollElements = document.querySelectorAll(".scroll__el")
 
-function makeLineActive(el){
-	id = el.target.id;
-	id = Number(id.replace(/[^+\d]/g, ''))-1;
-	document.querySelectorAll(".scroll__el-underline")[id].classList.add("scroll__el-underline-current")
+function getId(element){
+	let id = element.target.id
+	return Number(id.replace(/[^+\d]/g, ''))
 }
 
-function makeLineDisabled(el){
-	id = el.target.id;
-	id = Number(id.replace(/[^+\d]/g, ''))-1;
-	document.querySelectorAll(".scroll__el-underline")[id].classList.remove("scroll__el-underline-current")
+function addListenerForArray(elements, event, func){
+	for(let i=0;i!=elements.length;i++){
+		elements[i].addEventListener(event, func);
+	}
 }
 
+function deleteClassForIDs(currentId, elements, cl, maxId){
+	for(let i=0;i!=maxId;i++){
+		if(currentId!=i+1){
+			elements[i].classList.remove(cl);
+		}
+		continue;
+	}
 
-for(let i=0;i!=4;i++){
-	 scrollElements[i].addEventListener("mouseenter", makeLineActive)
-	 scrollElements[i].addEventListener("mouseleave", makeLineDisabled)
 }
+
+const makeLineActive = function(el){
+	document.querySelectorAll(".scroll__el-underline")[getId(el)-1].classList.add("scroll__el-underline-current")
+}
+
+const makeLineDisabled = function(el){
+	document.querySelectorAll(".scroll__el-underline")[getId(el)-1].classList.remove("scroll__el-underline-current")
+}
+
+addListenerForArray(scrollElements, "mouseenter", makeLineActive)
+addListenerForArray(scrollElements, "mouseleave", makeLineDisabled)
+
+const infoScreenScrollElements = document.querySelectorAll(".info__screen-scroll-el");
+
+const infoMakeLineActive = function(el){
+	document.querySelectorAll(".info__screen-scroll-el")[getId(el)-1].classList.add("info__screen-scroll-el_current")
+	deleteClassForIDs(getId(el), document.querySelectorAll(".info__screen-scroll-el"), "info__screen-scroll-el_current", 3)
+}
+
+addListenerForArray(infoScreenScrollElements, "click", infoMakeLineActive)
