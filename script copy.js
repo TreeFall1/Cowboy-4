abc75ce3d3e211d5687c066eb9ed5b8e4b9dbe5e
TreeFall1 <andreevdaniil06an@gmail.com>
2023-12-17 17:24:18 +0300

    Circle animation

--- script copy.js
+++ script copy.js
@@ -2,30 +2,37 @@ colorButton__blue=document.querySelectorAll("#circle__blue")[0]
 colorButton__khaki=document.querySelectorAll("#circle__khaki")[0]
 colorButton__black=document.querySelectorAll("#circle__black")[0]
 orderImage=document.querySelectorAll('.order__img')[0]
-currentCircle=document.querySelector(".current-circle")
+
 
 function bikeToBlue(){
 	orderImage.src="images/orderImage-Blue.png"
+	colorButton__blue.src="images/circle_blue-Current.svg";
 	colorButton__blue.classList.add("color-picker__circle-current")
 	colorButton__khaki.classList.remove("color-picker__circle-current")
 	colorButton__black.classList.remove("color-picker__circle-current")
-	currentCircle.style="border-color: #90b4fc; left: 175px"
+	colorButton__khaki.src="images/circle_khaki.svg"
+	colorButton__black.src="images/circle_black.svg"
+
 }
 
 function bikeToKhaki(){
 	orderImage.src="images/orderImage.png"
+	colorButton__khaki.src="images/circle_khaki-Current.svg";
 	colorButton__khaki.classList.add("color-picker__circle-current")
 	colorButton__blue.classList.remove("color-picker__circle-current")
 	colorButton__black.classList.remove("color-picker__circle-current")
-	currentCircle.style="border-color: #80847c;"
+	colorButton__blue.src="images/circle_blue.svg"
+	colorButton__black.src="images/circle_black.svg"
 }
 
 function bikeToBlack(){
 	orderImage.src="images/orderImage-Black.png"
+	colorButton__black.src="images/circle_black-Current.svg";
 	colorButton__black.classList.add("color-picker__circle-current")
 	colorButton__khaki.classList.remove("color-picker__circle-current")
 	colorButton__blue.classList.remove("color-picker__circle-current")
-	currentCircle.style="border-color: #000; left: 3px;"
+	colorButton__blue.src="images/circle_blue.svg"
+	colorButton__khaki.src="images/circle_khaki.svg"
 }
 
 colorButton__blue.addEventListener("click", bikeToBlue)
